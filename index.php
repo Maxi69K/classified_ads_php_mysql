fa6e8ee7d4a1d25e627952fb0f4db636a4a623cd
Goran Kekez <gorankekez@gmail.com>
2023-06-03 22:24:32 +0200

    fixed img

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['id'])) {
 }
 ?>
 <?php require_once "partials/navbar.php" ?>
-<div class="w-75 mx-auto mt-5">
+<div class="w-75 mx-auto mt-5 d-flex justify-content-center">
     <img src="https://source.unsplash.com/eveI7MOcSmw" class="img-fluid img-thumbnail" alt="Ads">
 </div>
 <?php require_once "partials/footer.php" ?>
